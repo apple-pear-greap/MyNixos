76a00383796b001e67824033f3469e0f16b0eebb
Cerydra <cerydrahysilens@qq.com>
2025-12-09 18:59:24 +0800

    Initial Nixos configuration

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "My NixOS Flake Configuration";
+
+    nixConfig = {
+    # 额外添加国内源
+    extra-substituters = [
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+    ];
+  };
+
+  inputs = {
+	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	daeuniverse.url = "github:daeuniverse/flake.nix";
+  };
+
+  outputs = { self, nixpkgs, daeuniverse, ... }@inputs: {
+	nixosConfigurations = {
+	nixos = nixpkgs.lib.nixosSystem {
+	  system = "x86_64-linux";
+	  specialArgs = { inherit inputs; };
+
+	  modules = [
+		./configuration.nix
+		daeuniverse.nixosModules.dae
+		daeuniverse.nixosModules.daed
+	  ];
+	};
+      };
+  };
+}
